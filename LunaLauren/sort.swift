c4942ed512d5db89a2c5d68016d218b2eaea984e
AixinLin <imlinaixin@gmail.com>
2018-07-13 00:13:04 -0400

    update

---
+++ LunaLauren/sort.swift
@@ -0,0 +1,15 @@
+//
+//  sort.swift
+//  LunaLauren
+//
+//  Created by 林蔼欣 on 2018-07-12.
+//  Copyright © 2018 Luna Cao. All rights reserved.
+//
+
+import Foundation
+
+
+class  User: Object {
+    @objc dynamic var name: String? = nil
+    var age = 
+}
