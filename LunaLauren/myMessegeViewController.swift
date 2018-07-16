3b276bb760151b2de0d3acd208c96d0b135454c4
AixinLin <imlinaixin@gmail.com>
2018-07-16 19:19:32 -0400

    chatting

--- LunaLauren/myMessegeViewController.swift
+++ LunaLauren/myMessegeViewController.swift
@@ -7,29 +7,23 @@
 //
 
 import UIKit
+//import SwiftKeychainWrapper
 
-class myMessegeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//class myMessegeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+//    @IBOutlet weak var tableView: UITableView!
+//    
+//    override func
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//    
 
-    /*
-    // MARK: - Navigation
+//}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
-}
+
