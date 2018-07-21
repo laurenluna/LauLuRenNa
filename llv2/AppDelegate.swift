a8a0b0c95ddf01aed9059c4adc77cecd05952642
Luna Cao <lunacao214@outlook.com>
2018-07-21 15:27:29 -0400

    1

--- llv2/AppDelegate.swift
+++ llv2/AppDelegate.swift
@@ -18,6 +18,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+       // let authListener = Auth.auth().addStateDidChangeListener(auth, user in
+         //   if user != nil{
+            
+           // }
+            //else{
+            
+//            })
+        
         return true
     }
 
